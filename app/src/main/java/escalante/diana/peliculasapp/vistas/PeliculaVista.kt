e5c19d317fc5bee42ddf04e3a6c6ab96392823e0
dianaescgar <a01253468@tec.mx>
2026-04-15 14:27:44 -0700

    Agregar TopAppBar

--- app/src/main/java/escalante/diana/peliculasapp/vistas/PeliculaVista.kt
+++ app/src/main/java/escalante/diana/peliculasapp/vistas/PeliculaVista.kt
@@ -8,20 +8,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import escalante.diana.peliculasapp.modelos.Pelicula
 import escalante.diana.peliculasapp.viewmodels.PeliculaViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PeliculaScreen(viewModel: PeliculaViewModel) {
     val peliculas = viewModel.pelicula.value
 
     Scaffold (
-
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Catálogo de Películas")
+                }
+            )
+        }
     ) { padding ->
         LazyColumn(
             modifier = Modifier
